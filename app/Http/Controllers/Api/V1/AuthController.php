f0f36834128d715c91b1d9db3fa66859e30308f1
Oleg Romanov <sqdcontrast@gmail.com>
2025-07-08 20:50:16 +0300

    feat(auth): implement login feature

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -4,8 +4,11 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
+use App\Http\Requests\LoginRequest;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\RegisterRequest;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
@@ -14,6 +17,26 @@ class AuthController extends Controller
         $credentials = $request->validated();
 
         $user = User::create($credentials);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token,
+        ]);
+    }
+
+    public function login(LoginRequest $request): JsonResponse
+    {
+        $credentials = $request->validated();
+
+        if (!Auth::attempt($credentials)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        $user = auth()->user();
 
         $token = $user->createToken('auth_token')->plainTextToken;
 
--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,5 @@ use App\Http\Controllers\Api\V1\AuthController;
 
 Route::prefix('v1')->group(function () {
     Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
 });
